5776fa1154bd8fa3f37db5d120583a0a7338b94a
Will Ley <wley3337@gmail.com>
2018-07-14 11:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-let newId(old) {1000000000 + old};
+let newId= function(old) {1000000000 + old};
 
 
