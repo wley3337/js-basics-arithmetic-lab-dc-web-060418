eed3aef030706c7a2e6ff8b1c405a1235571c21f
Will Ley <wley3337@gmail.com>
2018-07-14 12:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() *20;
 
-let randomInteger = Number.floor(randomNumber);
+let randomInteger = Math.floor(randomNumber);
 
 let randomUserID = 1000000000 + randomInteger;
