1b889e5dc0f98a635af835aa847dcc96c6829f03
Will Ley <wley3337@gmail.com>
2018-07-14 12:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-const newId = function(old) {1000000000 + oldId};
+const newId = 1000000000 + oldId;
 
 
